e478b913e84b1d821a824bd3c765c9dc0f689342
guygubaby <1907004005@qq.com>
2019-09-28 17:18:24 +0800

    [fix] fix the zhihu spider

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@ echo/ "start deploy ..."
 cd /
 mkdir -p hot-list || exit 1
 cd hot-list
-curl -o docker-compose.yml https://github.com/guygubaby/hot-list/raw/master/docker-compose.yml
+curl -o docker-compose.yml https://raw.githubusercontent.com/guygubaby/hot-list/master/docker-compose.yml
 docker-compose up -d
 echo "service start successful :)"
